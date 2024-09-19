60e911306ac7744ad07a931888d0b71990de1721
Sayali43 <sayalichoudhari43@gmail.com>
2024-09-20 01:54:12 +0530

    Bash Auto Push

---
+++ 004-variables.sh
@@ -0,0 +1,12 @@
+#! /bin/shell
+#variable is something that holds the data 
+a=10
+d=def
+# $ is a special varibale this helps you in printing the value of the varibale
+
+
+echo $a
+echo "the value of the varibale d is $d"
+echo "the value of the varibale d is ${d}"
+
+echo "the value of the varibale b is ${b}" 
