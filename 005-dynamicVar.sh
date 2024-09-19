699b57a8923549a5a3c976dda9d527c38062fbf4
Sayali43 <sayalichoudhari43@gmail.com>
2024-09-20 02:15:36 +0530

    Bash Auto Push

--- 005-dynamicVar.sh
+++ 005-dynamicVar.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-DATE="11/MARCH/2024
-echo - e "Todays DATE is $(date)
+#DATE="11/MARCH/2024
+#echo - e "Todays DATE is $(DATE)
 
-echo -e "Total opened sessions are $NO_OF_SESSIONS"
+#echo -e "Total opened sessions are $NO_OF_SESSIONS"
+
+DATE="$(date +%F)"
+NO_OF_SESSIONS ="Who | wc -l
+"
