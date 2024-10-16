d69215ade276f5e9f8684b3a7064e3fb6fa7818a
Sayali43 <sayalichoudhari43@gmail.com>
2024-10-16 16:17:41 +0530

    Bash Auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -6,15 +6,34 @@ AMI_ID="ami-0bee47ea953515411"
 SGID="sg-009ab1454a519d038"               # Create your own Security Group that allows allows all and then add your SGID 
 HOSTEDZONE_ID="Z085783623DWXDG27CQ0U"     # User your private zone id
 COMPONENT=$1
+ENV=$2
+COLOR="\e[35m"
+NOCOLOR="\e[0m"
 
 
-if [ -z "$1" ]; then
-    echo -e "\e[31m   COMPONENT IS NEEDED: \e[0m"
-    echo -e "\e[36m \t\t Example Usage : \e[0m  bash launch-ec2.sh dev ratings"
+if [ -z $1 ] || [ -z $2 ] ; then
+    echo -e "\e[31m   COMPONENT & ENV ARE NEEDED: \e[0m"
+    echo -e "\e[36m \t\t Example Usage : \e[0m  bash launch-ec2 dev ratings"
     exit 1
+fi 
+
+create_ec2() {
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq .Instances[].PrivateIpAddress |sed -e 's/"//g')
+    echo -e "___ $COLOR $1-$2 Server Created and here is the IP ADDRESS $PRIVATE_IP $NOCOLOR ___"
+
+    echo "Creating r53 json file with component name and ip address:"
+    sed -e "s/IPADDRESS/${PRIVATE_IP}/g" -e "s/COMPONENT/${COMPONENT}-${ENV}/g" route53.json  > /tmp/dns.json 
+
+    echo -e "___ $COLOR Creating DNS Record for $COMPONENT-${ENV} ___ $NOCOLOR \n\n"
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/dns.json 
+}
+
+# if component name from user is all, then I would like create & update all 10 servers and it's DNS Records 
+if [ "$1" == "all" ]; then 
+    for comp in frontend mongodb catalogue user redis cart mysql shipping rabbitmq payment; do 
+        COMPONENT=$comp
+        create_ec2
+    done
+else  
+    create_ec2
 fi
-
- PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=\"${COMPONENT}\"}]" | jq .Instances[].PrivateIpAddress |sed -e 's/"//g')
- echo "$1 Server Created and here is the IP ADDRESS of $PRIVATE_IP"
-
- 
